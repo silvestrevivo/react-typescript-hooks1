1083b46e2465bfe4f3584ed06991ee10648ef72c
silvestrevivo <silvestrevivomillan@gmail.com>
2019-07-16 00:32:37 +0200

    adding and completing todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-type Props = {
+interface Props {
   title: string
 }
 
+interface ITodo {
+  text: string,
+  complete: boolean
+}
+
+type FormElement = React.FormEvent<HTMLFormElement>;
+
 function App({ title }: Props): JSX.Element {
+  const [value, setValue] = useState<string>('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElement): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue('');
+  }
+
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }]
+    setTodos(newTodos);
+  }
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
-      {title}
+      <h1>{title}</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={e => setValue(e.target.value)} required />
+        <button type="submit">Add Todo</button>
+      </form>
+      <section>
+        {todos.map((todo: ITodo, index: number): JSX.Element => {
+          return (
+            <React.Fragment key={todo.text}>
+              <div style={{ textDecoration: todo.complete ? 'line-through' : '' }}>{todo.text}</div>
+              <button type="button" onClick={() => completeTodo(index)}>
+                {todo.complete ? 'Incomplete' : 'Complete'}
+              </button>
+            </React.Fragment>
+          )
+        })}
+      </section>
     </div>
   );
 }
